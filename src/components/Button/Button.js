d7bb338e1f070eabccff4dc4d0c4d428a1b17174
Fabian Regnery <fabian.regnery@icloud.com>
2021-08-29 07:30:02 +0200

    Changes in Button

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -4,15 +4,8 @@ import { useState, useEffect } from "react";
 
 function Button(props) {
 
-  function next(){
-    props.setIndex(props.index + 1)
-  }
-  function prev(){
-    props.setIndex(props.index - 1)
-  }
-
   return (
-    <button onClick={props.func}> {props.name} {props.children} {props.index}</button>
+    <button onClick={props.func}> {props.name} {props.children}</button>
   )
 }
 
